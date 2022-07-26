6115e4ed4795b643b045a6c4d89da35b35b87063
Sythe007 <mrtam7777@gmail.com>
2022-07-26 17:42:01 +0700

    emotion-detection

---
+++ emotion_classifier.py
@@ -0,0 +1,160 @@
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+#importing modules and these models can be used for prediction and feature extraction and fine tuning
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization
+from keras.layers import Conv2D,MaxPooling2D
+
+import os
+
+num_classes = 7
+
+img_rows, img_cols = 48, 48
+
+batch_size = 32
+
+train_data_dir = r'D:\Kuliah\Semester 4\Image Processing\fer2013\train'
+validation_data_dir = r'D:\Kuliah\Semester 4\Image Processing\fer2013\test'
+
+#block of train data generation which generates many images 
+#from a single image which will help in training machine and 
+#improve their accuracy
+train_datagen = ImageDataGenerator(
+                    rescale=1./255,
+                    rotation_range=30,
+                    shear_range=0.3,
+                    zoom_range=0.3,
+                    width_shift_range=0.4,
+                    height_shift_range=0.4,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+
+train_generator = train_datagen.flow_from_directory(
+                    train_data_dir,
+                    color_mode='grayscale',
+                    target_size=(img_rows, img_cols),
+                    batch_size=batch_size,
+                    class_mode='categorical',
+                    shuffle=True)
+
+validation_generator = validation_datagen.flow_from_directory(
+                            validation_data_dir,
+                            color_mode='grayscale',
+                            target_size=(img_rows, img_cols),
+                            batch_size=batch_size,
+                            class_mode='categorical',
+                            shuffle=True)
+
+model = Sequential()
+
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0,2))
+
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0,2))
+
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0,2))
+
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0,2))
+
+#this block of codes will flatten the network where it will not have a matrix 
+model.add(Flatten())
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+#The above block of codes and the below block of codes will be 
+# fully connected network having 64 neuron having dropout of 50%
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+model.add(Dense(num_classes,kernel_initializer='he_normal'))
+model.add(Activation('softmax'))
+print(model.summary())
+
+#importing optimizer 
+from tensorflow.keras.optimizers import RMSprop,SGD,Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+#Saving model Checkpoint
+checkpoint = ModelCheckpoint('datacollect.h5',
+                             monitor='val_loss',
+                             mode='min',
+                             save_best_only=True,
+                             verbose=1)
+#earlystop will be applied for where there is no improvement in validation loss upto 3 epochs
+earlystop = EarlyStopping(monitor='val_loss',
+                          min_delta=0,
+                          patience=3,
+                          verbose=1,
+                          restore_best_weights=True)
+#reduce_lr will also monitor the validation loss 
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=3,
+                              verbose=1,
+                              min_delta=0.0001)
+
+callbacks = [earlystop, checkpoint, reduce_lr]
+model.compile(loss='categorical_crossentropy',
+              optimizer = Adam(learning_rate=0.001),
+              metrics=['accuracy'])
+
+nb_train_samples = 28789
+nb_validation_samples = 7178
+epochs=25
+
+history=model.fit(
+                train_generator,
+                steps_per_epoch=nb_train_samples//batch_size,
+                epochs=epochs,
+                callbacks=callbacks,
+                validation_data=validation_generator,
+                validation_steps = nb_validation_samples//batch_size)
+
+plt.style.use('dark_background')
+
+plt.figure(figsize=(20,10))
+plt.subplot(1, 2, 1)
+plt.suptitle('Optimizer : Adam', fontsize=10)
+plt.ylabel('Loss', fontsize=16)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend(loc='upper right')
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy', fontsize=16)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.show()
